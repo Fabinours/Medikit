24b0447649868d1e8cbe5a67e3dcb477725dde47
Fabinours <44270057+Fabinours@users.noreply.github.com>
2020-01-20 20:07:02 +0100

    Brouillons&FileEditor INIT

---
+++ Assets/Scripts/Pages/BrouillonsScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrouillonsScript : MonoBehaviour
+{
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PublishDraft()
+    {
+
+    }
+
+    public void ModifyDraft()
+    {
+
+    }
+
+    public void DeleteDraft()
+    {
+
+    }
+
+    public void SearchDrafts()
+    {
+
+    }
+
+    public void SearchAllDrafts()
+    {
+
+    }
+}
