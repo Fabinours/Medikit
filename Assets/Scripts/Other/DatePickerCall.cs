3504292f6364007b8b78a927499ca706831d9622
Katastorm <45739730+katastorm@users.noreply.github.com>
2020-02-02 14:35:58 +0100

    datepickerfield for davy

---
+++ Assets/Scripts/Other/DatePickerCall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DatePickerCall : MonoBehaviour
+{
+    [SerializeField]
+    int defaultYear = 2000;
+    [SerializeField]
+    int defaultMonth = 01;
+    [SerializeField]
+    int defaultDay = 01;
+
+
+
+    InputField input;
+   
+
+
+    void Awake()
+    {
+        input = GetComponentInChildren<InputField>();
+        input.text = defaultYear + "/" + defaultMonth + "/" + defaultDay;
+    }
+
+    public void OnClick()
+    {
+        DatePickerControl.OpenPicker((s) => input.text = s); ;
+    }
+
+    public string GetValue()
+    {
+        return input.text;
+    }
+
+    }
